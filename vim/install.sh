3e0f3cb7e521c7ad1fffd9235eeb0dceb869bd91
Andre Oliveira <andreoliveirar2d2@gmail.com>
2021-03-10 21:35:13 -0300

    fix: the if condition of colors folder was wrong

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,4 @@
 sudo apt-get install neovim -y
-echo "alias vim=nvim" >> ~/.zshrc
 
 mkdir -p ~/.config/nvim
 mv ./.vimrc ~/.config/nvim/init.vim
@@ -7,6 +6,6 @@ mv ./.vimrc ~/.config/nvim/init.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if ! mkdir ~/.config/nvim/colors then;
+if [! mkdir ~/.config/nvim/colors] then;
   'the ~/.vim/colors already exists'
 fi
